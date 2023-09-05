1d4c7020986d6d5f1b6750121128e5d748165d71
Mijael Yen Quispe <mijael.yq2002@outlook.com>
2023-09-04 21:50:33 -0500

    CONTACT-FORM

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,16 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import ContactCmp from "@/components/contacto-cmp.vue";
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
   </header>
 
   <main>
-    <TheWelcome />
+    <contact-cmp/>
   </main>
 </template>
 
