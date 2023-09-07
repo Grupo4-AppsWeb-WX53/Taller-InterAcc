2c41f4d3ebd0b35337af1fa3d92f41b656f1d6f6
Mijael Yen Quispe <mijael.yq2002@outlook.com>
2023-09-06 20:15:45 -0500

    configuracion inicial - con button

--- src/components/locales/i18n.js
+++ src/components/locales/i18n.js
@@ -4,14 +4,10 @@ const i18n = createI18n({
     locale: 'es',
     messages: {
         en: {
-            "category": 'category',
             "contact": 'turororial',
-            "greet": 'Hello, {user}'
         },
         es: {
-            "category": 'Categoria',
             "contact": 'FORM DE CONTACTO',
-            "greet": 'Hola, {user}'
         }
     }
 })
--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import i18n from "@/components/locales/i18n";
+import PrimeVue from 'primevue/config';
+import Button from "primevue/button";
 
 const app = createApp(App);
 app.use(i18n);
+app.use(PrimeVue);
+app.component('Button', Button);
 app.mount('#app')
